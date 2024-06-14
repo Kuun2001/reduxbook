78f1f8ac04d1efa5fd89045f802621f07590f55b
Kuun <hongkimnhut2001@gmail.com>
2024-06-15 03:27:07 +0700

    commit

---
+++ src/features/books/bookSlice.js
@@ -0,0 +1,95 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify';
+import api from '../../apiService';
+
+const initialState = {
+    books: [],
+    status: "idle",
+    errorMessage: "",
+    loading: false,
+    book: {}
+}
+
+export const getBooks = createAsyncThunk(
+    "books/getBooks",
+    async ({ pageNum, limit, query }, thunkApi) => {
+        let url = `/books?_page=${pageNum}&_limit=${limit}`;
+        if (query) url += `&q=${query}`;
+        const res = await api.get(url);
+        return res.data
+    }
+)
+
+export const getSingleBook = createAsyncThunk(
+    "books/getSingleBook",
+    async ({ bookId }) => {
+        const res = await api.get(`/books/${bookId}`);
+        return res.data
+    }
+)
+
+export const addFavorite = createAsyncThunk(
+    "books/addFavorite",
+    async ({ addingBook }) => {
+        await api.post(`/favorites`, addingBook);
+        toast.success("The book has been added to the reading list!");
+    }
+)
+
+
+const bookSlice = createSlice({
+    name: "books",
+    initialState,
+    reducer: {},
+    extraReducers: (builder) => {
+        builder.addCase(getBooks.pending, (state, action) => {
+            //action = {type:"books/getBooks/pending, payload: [{tile:..}]}
+            state.status = "loading"
+            state.loading = true
+            state.errorMessage = ""
+        })
+            .addCase(getBooks.fulfilled, (state, action) => {
+                //action = {type:"books/getBooks/fulfilled", payload: [{tile:..}]}
+                state.status = "idle"
+                state.loading = false
+                state.books = action.payload
+            })
+            .addCase(getBooks.rejected, (state, action) => {
+                //action = {type:"books/getBooks/rejected", payload: {error:{message:"fail"}}}
+                state.status = "fail"
+                state.loading = false
+                state.errorMessage = action.error.message
+            })
+        builder.addCase(getSingleBook.pending, (state) => {
+            state.status = "loading"
+            state.loading = true
+            state.errorMessage = ""
+        })
+            .addCase(getSingleBook.fulfilled, (state, action) => {
+                state.status = "idle"
+                state.loading = false
+                state.book = action.payload
+            })
+            .addCase(getSingleBook.rejected, (state, action) => {
+                state.status = "fail"
+                state.loading = false
+                state.errorMessage = action.error.message
+            })
+        builder.addCase(addFavorite.pending, (state) => {
+            state.status = "loading"
+            state.loading = true
+            state.errorMessage = ""
+        })
+            .addCase(addFavorite.fulfilled, (state, action) => {
+                state.status = "idle"
+                state.loading = false
+            })
+            .addCase(addFavorite.rejected, (state, action) => {
+                state.status = "fail"
+                state.loading = false
+                state.errorMessage = action.error.message
+            })
+    }
+})
+
+export default bookSlice.reducer
